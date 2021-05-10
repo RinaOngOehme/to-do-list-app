976d70a5ab0b521cd53583cab52d83cb7480982f
RinaOngOehme <81406377+RinaOngOehme@users.noreply.github.com>
2021-05-10 19:33:22 +0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,32 @@
 function newItem() {
-//Add a new item to the list:
-  let li = $('<li></li>');
+  //Add a new item to the list:
+  let li = $("<li></li>");
   let inputValue = $('#input').val();
   li.append(inputValue);
 
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    $('#list').append(li);
-  }
-};
-//Cross an item out:
-  function crossOut() {
-		li.toggleClass("strike");
-	}
-
- let li = $('<li></li>');
-	li.on("dblclick", function crossOut() {
-		li.toggleClass("strike");
-	});
-//Add a delete button
-  let crossOutButton = $('<crossOutButton></crossOutButton>');
-  crossOutButton.append(document.createTextNode('X'));
+  let crossOutButton = $("<crossOutButton></crossOutButton>");
+  crossOutButton.append(document.createTextNode("X"));
   li.append(crossOutButton);
 
-//sort list
-   $('#list').sortable();
+  li.on("dblclick", function crossOut() {
+		li.toggleClass("strike");
+	});
+
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    $("#list").append(li);
+  };
+
+  //remove an item
+  $("crossOutButton").on('click', function() {
+        li.remove();
+      });
+  //Cross an item out:
+  function crossOut() {
+    li.toggleClass("strike");
+	  }
+
+  //sort list
+   $("#list").sortable();
+}
